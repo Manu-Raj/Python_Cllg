a9a2d4d0ffef932dc2a8e3bd7407e1f81a91b8b7
Manu-Raj <maniishraj10425@yahoo.com>
2024-10-26 22:48:44 +0530

    Updated sales.py

--- Python_Projects/W5/sales.py
+++ Python_Projects/W5/sales.py
@@ -1,8 +1,9 @@
 sales_Rec=[25000,30000,7500,50350,-46555,12900,75000,96000,84525,65412,78545,33600,74525,68544,65421]
 
-for i in sales_Rec:
-    if i<0:
-        sales_Rec.remove(i)
+for i in range(len(sales_Rec)):
+    if sales_Rec[i]<0:
+        sales_Rec.remove(sales_Rec[i])
+        sales_Rec.insert(i,0)
 
 print("The Average sales",(sum(sales_Rec)/len(sales_Rec)))
 print("Maximum Sales on day ",sales_Rec.index(max(sales_Rec))+1)
